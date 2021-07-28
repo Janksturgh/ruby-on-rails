57901b02fcecb85fec1b5d7b7fd48463311157a0
Jai Barlow <jai.barlow@education.govt.nz>
2021-07-28 15:43:06 +1200

    updating scraper comments

--- web-scraper/nba_scraper.rb
+++ web-scraper/nba_scraper.rb
@@ -1,28 +1,34 @@
 #requiring gem packages
+
+#open-uri is a wrapper for Net::HTTP, Net::HTTPS and Net::FTP.
 require 'open-uri'
+#nokogiri is an API for querying documents
 require 'nokogiri'
+#csv is a library that provides an interface to CSV files and data
 require 'csv'
 
 #creating a variable for the url
 url = "http://en.wikipedia.org/wiki/List_of_current_NBA_team_rosters"
 
 
-#creating a variable which will scrape the page
+#creating a variable which will scrape the page using Nokogiri
 page = Nokogiri::HTML(URI.open(url))
 
-#put string is calling the page variable and scraping for the outline li.toclevel-3 html, 
+#puts(put string) is calling the page variable and scraping for the outline li.toclevel-3 html, 
 # not sure what the .css part is about, and then .text is a ruby method that looks at the line of code
 # and pulls out the text from that
 # puts page.css('li.toclevel-3 span.toctext').text
 
 
 #each is a special ruby method along with do and line creates a loop
-#and the puts inside of it is what is in the loop
+#and the puts(put string) inside of it is what is in the loop
 #end finishes the loop
 page.css('li.toclevel-3 span.toctext').each do |line|
   puts line.text
 end
 
+# here we can select a table cell (<td>) and chose all table cells that have an inline style of
+#text-align: left; and also occurs as the third <td> in a <tr>
 page.css('td[style="text-align:left;"][3] a').each do |line|
   puts line.text
 end
